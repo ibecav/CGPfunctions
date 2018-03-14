933c5ded18f8936f43014e87d63fdec8f94b8df9
Chuck Powell <cpowell@cpowell-869228.local>
2018-03-14 15:31:05 -0400

    xxx

--- R/Mode.R
+++ R/Mode.R
@@ -9,13 +9,21 @@
 #' @return a vector containing one or more modal values for the input vector
 #' @export
 #'
+#' @section Warning:
+#' Be careful the function does some basic error checking but the return to
+#' \code{Mode(NA)} is \code{NA} and a vector where the majority of entries
+#' are \code{NA} is also NA
+#' 
 #' @examples
 #' Mode(sample(1:100,1000,replace=TRUE))
 #' Mode(mtcars$hp)
 #' Mode(iris$Sepal.Length)
 #' 
 Mode <- function(x) {
-  # error checking
+  # error checking   
+  if (missing(x)) {
+    stop("Argument \"x\" is missing, with no default")
+  }
   if (!is.vector(x)) {
     stop("I can only process a vector of data")
   }
