6c5b15b17b90bbf10086d272d12e00b2356b93d5
Chuck Powell <cgpowell@Chucks-MacBook-Pro.local>
2018-03-14 21:23:31 -0400

    xxx

--- vignettes/CGPfunctions.R
+++ vignettes/CGPfunctions.R
@@ -4,9 +4,6 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(CGPfunctions::neweta(aov(mpg~am*cyl, mtcars)))
-
 ## ----echo=FALSE, message=FALSE, warning=FALSE, paged.print=FALSE---------
 xxx<-CGPfunctions::Plot2WayANOVA(mpg~am*cyl, mtcars)
 
