813a68eea4a04b44f40fec00b87558891e620c44
Chuck Powell <cpowell@cpowell-869228.local>
2018-03-16 10:50:55 -0400

    xxx

--- R/seedist.R
+++ R/seedist.R
@@ -69,7 +69,7 @@ SeeDist <- function (qqq, numbins = 0, whatvar = "Unspecified")
     labs(title = paste0("Distribution of the variable ", xxx, " (", whatvar, ")"),
          subtitle = bquote("N ="~.(length(qqq))*","~bar(X)~"="~.(round(meanqqq,1))*", SD ="~.(round(sdqqq,2))*", Median ="~.(round(medianqqq,2))*", Skewness ="~.(round(Skewqqq,2))*", Kurtosis ="~.(round(Kurtosisqqq,2))),
          x = whatvar,
-         caption = (bquote(bar(X)~" is a green line, Median is a yellow line, Mode(s) as orange line(s), Black curve density plot, Red curve is normal"))) +
+         caption = (bquote(bar(X)~" = green, Median = yellow, Mode(s) = orange, Black = density plot, Red = theoretical normal"))) +
     xlim(-3 * sd(qqq) + mean(qqq),3 * sd(qqq) + mean(qqq)) +
     theme(axis.title.y=element_blank(),
           axis.text.y=element_blank(),
