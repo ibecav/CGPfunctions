64071eb4c6f8a9f68401e78ff4b49d1cfc8cceb3
Chuck Powell <cpowell@cpowell-869228.local>
2018-03-19 13:06:03 -0400

    xxx

---
+++ R/CGPfunctions.R
@@ -0,0 +1,15 @@
+#' CGPfunctions: A package for of miscellaneous functions for teaching statistics.
+#'
+#' A package that includes miscellaneous functions useful for teaching statistics as well as actually practicing the art. They typically are not “new” methods but rather wrappers around either base R or other packages.
+#' 
+#' @section Functions included:
+#' \itemize{
+#'   \item \code{Plot2WayANOVA} which as the name implies conducts a 2 way ANOVA and plots the results using `ggplot2`
+#'   \item \code{neweta} which is a helper function that appends the results of a Type II eta squared calculation onto a classic ANOVA table
+#'   \item \code{Mode} which finds the modal value in a vector of data
+#'   \item \code{SeeDist} which wraps around ggplot2 to provide visualizations of univariate data.
+#' }
+#'
+#' @docType package
+#' @name CGPfunctions
+NULL
