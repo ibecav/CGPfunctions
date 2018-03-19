d1593776c63d07055ddfb281f2cae28ba4beae49
Chuck Powell <cgpowell@Chucks-MacBook-Pro.local>
2018-03-18 21:24:26 -0400

    xxx

--- R/seedist.R
+++ R/seedist.R
@@ -115,7 +115,7 @@ SeeDist <- function (qqq, numbins = 0, whatvar = "Unspecified", whatplots = c("d
          x = whatvar,
          caption = (bquote(bar(X)~" displayed as a green line, Median as a yellow line, and Mode(s) as orange line(s)"))) +
     geom_histogram(bins=binnumber,color = "black",fill="deepskyblue") +
-    geom_vline(xintercept = meanqqq, colour="dark green", linetype="dashed", size=1.5) +
+    geom_vline(xintercept = meanqqq, colour="dark green", linetype="longdash", size=1.5) +
     geom_vline(xintercept = medianqqq, colour="yellow", linetype="dashed", size=1.5) +
     geom_vline(xintercept = modeqqq, colour="orange", linetype="dashed") 
     print(ppp)
