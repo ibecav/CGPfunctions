f456f71cf046313e6b3ab0e26dd58b96925cf376
Chuck Powell <cgpowell@Chucks-MacBook-Pro.local>
2018-03-19 20:56:58 -0400

    xxx

--- vignettes/CGPfunctions.R
+++
@@ -1,9 +0,0 @@
-## ----setup, include = FALSE----------------------------------------------
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>"
-)
-
-## ----echo=FALSE, message=FALSE, warning=FALSE, paged.print=FALSE---------
-xxx<-CGPfunctions::Plot2WayANOVA(mpg~am*cyl, mtcars)
-
