3350f37210d31451914b1541623407de1300e8f5
Chuck Powell <cpowell@00e04c680024.faculty.trincoll.edu>
2018-04-19 15:15:00 -0400

    examples

--- R/seedist.R
+++ R/seedist.R
@@ -30,8 +30,8 @@
 #' 
 #' @examples
 #' SeeDist(rnorm(100, mean=100, sd=20), numbins = 15, whatvar = "A Random Sample")
-#' SeeDist(mtcars$hp, whatvar = "Horsepower")
-#' SeeDist(iris$Sepal.Length, whatvar = "Sepal Length")
+#' SeeDist(mtcars$hp, whatvar = "Horsepower", whatplots = c("d","b"))
+#' SeeDist(iris$Sepal.Length, whatvar = "Sepal Length", whatplots = "d")
 #' 
 #' @author Chuck Powell
 #' 
