59ee524250177b92a7bbbc8fe3ce342bed38aeff
Chuck Powell <cpowell@00e04c680024.faculty.trincoll.edu>
2018-06-11 12:35:09 -0400

    initial commit

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Tufte dataset on cancer survival rates
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 96 rows and 3 variables:
+#' \describe{
+#'   \item{Year}{ordered factor for the 5, 10, 15 and 20 year survival rates}
+#'   \item{Type}{factor containing the name of the cancer type}
+#'   \item{Survival}{numeric for this data a whole number corresponding to the percent survival rate}
+#'   
+#' }
+#' @source \url{https://www.edwardtufte.com/bboard/q-and-a-fetch-msg?msg_id=0003nk}
+"newcancer"
