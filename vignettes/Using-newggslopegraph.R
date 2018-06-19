3a86b538e01d529215a7b721d401431c4730b6bd
Chuck Powell <ibecav@gmail.com>
2018-06-19 12:03:08 -0400

    vignette rebuilt

--- vignettes/Using-newggslopegraph.R
+++ vignettes/Using-newggslopegraph.R
@@ -58,12 +58,6 @@ newggslopegraph(moredata, Date, Pct, Party,
                 )
 
 ## ----ggslope6, fig.height=12, fig.width=6--------------------------------
-newgdp <- structure(list(Year = c("Year1970", "Year1979", "Year1970", "Year1979",  "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979",  "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979", "Year1970", "Year1979"), 
-               Country = structure(c(1L, 1L, 2L, 2L, 3L, 3L, 4L, 4L, 5L, 5L, 6L, 6L, 7L, 7L, 8L, 8L, 9L, 9L, 10L, 10L, 11L, 11L, 12L, 12L, 13L, 13L, 14L, 14L, 15L, 15L), 
-              .Label = c("Sweden", "Netherlands", "Norway", "Britain", "France", "Germany", "Belgium", "Canada", "Finland", "Italy", "US", "Greece", "Switzerland", "Spain", "Japan"), class = "factor"), 
-               GDP = c(46.9, 57.4, 44, 55.8, 43.5, 52.2, 40.7, 39, 39, 43.4, 37.5, 42.9, 35.2, 43.2, 35.2, 35.8, 34.9, 38.2, 30.4, 35.7, 30.3, 32.5, 26.8, 30.6, 26.5, 33.2, 22.5, 27.1, 20.7, 26.6)), 
-              row.names = c(NA, -30L), class = "data.frame")
-
 newggslopegraph(newgdp, 
                 Year, 
                 GDP, 
