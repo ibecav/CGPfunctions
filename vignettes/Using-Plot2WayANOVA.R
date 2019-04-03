0f5deea55ca001a619e90c0869c69623aef0107e
Chuck Powell <ibecav@gmail.com>
2019-04-03 11:52:18 -0400

    Vignette update

--- vignettes/Using-Plot2WayANOVA.R
+++ vignettes/Using-Plot2WayANOVA.R
@@ -15,6 +15,38 @@ knitr::opts_chunk$set(
 ## ----LoadLibrary, warning = FALSE----------------------------------------
 library(CGPfunctions)
 
-## ----Plot2WayANOVA, echo=TRUE, message=TRUE, warning=FALSE, fig.width=6.0, fig.height=3.5----
+## ----Plot2WayANOVA, echo=TRUE, message=TRUE, warning=FALSE, fig.width=7, fig.height=4----
 Plot2WayANOVA(formula = mpg ~ am * cyl, dataframe = mtcars)
+
+## ----Plot2WayANOVA2, echo=TRUE, fig.height=4, fig.width=7, message=FALSE, warning=FALSE----
+Plot2WayANOVA(formula = mpg ~ cyl * am, 
+              dataframe = mtcars,
+              confidence = .99,
+              title = "MPG by cylinders and type transmission",
+              xlab = "Cylinders",
+              ylab = "Miles per gallon",
+              mean.label = TRUE,
+              mean.shape = 22,
+              posthoc.method = "lsd")
+
+## ----Plot2WayANOVA3, echo=TRUE, fig.height=4, fig.width=7, message=FALSE, warning=FALSE----
+# Create a new dataset
+library(dplyr)
+library(ggplot2)
+library(stringi)
+newmpg <- mpg %>% 
+          filter(cyl != 5) %>% 
+          mutate(am = stringi::stri_extract(trans, regex = "auto|manual"))
+Plot2WayANOVA(formula = hwy ~ am * cyl,
+              dataframe = newmpg,
+              ylab = "Highway mileage",
+              xlab = "Transmission type",
+              plottype = "line",
+              offset.style = "wide",
+              overlay.type = "box",
+              mean.label = TRUE, 
+              mean.shape = 20, 
+              mean.size = 3, 
+              mean.label.size = 3,
+              show.dots = TRUE)
 
