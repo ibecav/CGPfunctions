9c057889bf3067d7251f1a846f4c3b16802933af
Chuck Powell <ibecav@gmail.com>
2020-03-26 13:16:06 -0400

    Moving pval function to helpers

--- R/helpers.R
+++ R/helpers.R
@@ -83,3 +83,27 @@ bf_display <- function(bf = NULL,
 }
 
 
+### -----  internal function to format p values =====================
+
+pvalr <- function(pvals, sig.limit = .001, digits = 3, html = FALSE) {
+  
+  roundr <- function(x, digits = 1) {
+    res <- sprintf(paste0('%.', digits, 'f'), x)
+    zzz <- paste0('0.', paste(rep('0', digits), collapse = ''))
+    res[res == paste0('-', zzz)] <- zzz
+    res
+  }
+  
+  sapply(pvals, function(x, sig.limit) {
+    if (x < sig.limit)
+      if (html)
+        return(sprintf('&lt; %s', format(sig.limit))) else
+          return(sprintf('< %s', format(sig.limit)))
+    if (x > .1)
+      return(roundr(x, digits = 2)) else
+        return(roundr(x, digits = digits))
+  }, sig.limit = sig.limit)
+}
+
+
+
