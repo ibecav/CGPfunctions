c52a8f133fdf1daf1103b35467dbbea16db908fc
Chuck Powell <ibecav@gmail.com>
2020-04-01 17:18:39 -0400

    Lots of updates for seedist CRAN submission

--- R/seedist.R
+++ R/seedist.R
@@ -15,20 +15,40 @@
 #'   specifies line color (Default: `darkgreen`, `yellow`, `orange`).
 #' @param mean.line.type,median.line.type,mode.line.type Character string that 
 #'   specifies line color (Default: `longdash`, `dashed`, `dashed`).
-#' @param whatplots what type of plots?  The default is whatplots = c("d","b","h")
-#'   for a density, a boxplot, and a histogram
+#' @param mean.line.size,median.line.size,mode.line.size Numeric that 
+#'   specifies line size (Default: `1.5`, `1.5`, `1`).  You can set to `0` to make
+#'   any of the lines "disappear".
+#' @param mean.point.shape,median.point.shape Integer in 0 - 25 
+#'   specifies shape of mean or median point mark on the violin plot
+#'   (Default: `21`, `23`).
+#' @param mean.point.size,median.point.size Integer  
+#'   specifies size of mean or median point mark on the violin plot
+#'   (Default: `4`). You can set to `0` to make any of the points "disappear".
+#' @param zcurve.color,tcurve.color Character string that 
+#'   specifies line color (Default: `red`, `black`).
+#' @param zcurve.type,tcurve.type Character string that 
+#'   specifies line color (Default: `twodash`, `dotted`).
+#' @param zcurve.size,tcurve.size Numeric that 
+#'   specifies line size (Default: `1`).  You can set to `0` to make
+#'   any of the lines "disappear".
+#' @param whatplots what type of plots?  The default is whatplots = c("d", "b", "h", "v")
+#'   for a density, a boxplot, a histogram, and a violin plot
 #' @param xlab Custom text for the `x` axis label (Default: `NULL`, which
 #'   will cause the `x` axis label to be the `x` variable).
 #' @param k Number of digits after decimal point (should be an integer)
 #'   (Default: k = 2) for statistical results.
+#' @param ggtheme A function, ggplot2 theme name. Default value is ggplot2::theme_bw().
+#'   Any of the ggplot2 themes, or themes from extension packages are allowed (e.g.,
+#'   hrbrthemes::theme_ipsum(), etc.).
 #'
-#' @return from 1 to 3 plots depending on what the user specifies as well as a
-#'   base R summary printed to the console
+#' @return from 1 to 4 plots depending on what the user specifies as well as an
+#'   extensive summary courtesy `DescTools::Desc` printed to the console
 #'
 #' @export
 #' @import ggplot2
 #' @importFrom grDevices nclass.FD
 #' @importFrom stats dnorm dt median sd
+#' @importFrom DescTools Desc
 #'
 #' @section Warning:
 #'   If the data has more than 3 modal values only the first three of them are plotted.
@@ -55,10 +75,26 @@ SeeDist <- function(x,
                     mean.line.type = "longdash",
                     median.line.type = "dashed",
                     mode.line.type = "dashed",
-                    whatplots = c("d", "b", "h"),
-                    k = 2) {
+                    mean.line.size = 1.5,
+                    median.line.size = 1.5,
+                    mean.point.shape = 21,
+                    median.point.shape = 23,
+                    mean.point.size = 4,
+                    median.point.size = 4,
+                    zcurve.color = "red",
+                    zcurve.type = "twodash",
+                    zcurve.size = 1,
+                    tcurve.color = "black",
+                    tcurve.type = "dotted",
+                    tcurve.size = 1,
+                    mode.line.size = 1,
+                    whatplots = c("d", "b", "h", "v"),
+                    k = 2,
+                    ggtheme = ggplot2::theme_bw()
+                    ) {
 
-  theme_set(theme_bw())
+  # set default theme 
+  ggplot2::theme_set(ggtheme)
   
   if (!is.numeric(x)) {
     stop("Sorry the data must be numeric")
@@ -146,12 +182,16 @@ SeeDist <- function(x,
       aes(x) +
       geom_density(fill = data.fill.color, ) +
       stat_function(fun = dnorm, 
-                    color = "red", 
+                    color = zcurve.color, 
+                    linetype = zcurve.type,
+                    size = zcurve.size,
                     args = list(mean = x_mean, 
                                 sd = x_sd)
                     ) +
       stat_function(fun = custom_t_function, 
-                    color = "black",
+                    color = tcurve.color,
+                    linetype = tcurve.type,
+                    size = tcurve.size,
                     args = list(mu = x_mean, 
                               nu = x_sd, 
                               df = length(x) - 1, 
@@ -160,14 +200,15 @@ SeeDist <- function(x,
       geom_vline(xintercept = x_mean, 
                  colour = mean.line.color, 
                  linetype = mean.line.type, 
-                 size = 1.5) +
+                 size = mean.line.size) +
       geom_vline(xintercept = x_median, 
                  colour = median.line.color, 
                  linetype = median.line.type, 
-                 size = 1.5) +
+                 size = median.line.size) +
       geom_vline(xintercept = x_mode, 
                  colour = mode.line.color, 
-                 linetype = mode.line.type) +
+                 linetype = mode.line.type,
+                 size = mode.line.size) +
       geom_rug(aes(y = 0)) +
       labs(
         title = my_title,
@@ -238,15 +279,57 @@ SeeDist <- function(x,
       geom_vline(xintercept = x_mean, 
                  colour = mean.line.color, 
                  linetype = mean.line.type, 
-                 size = 1.5) +
+                 size = mean.line.size) +
       geom_vline(xintercept = x_median, 
                  colour = median.line.color, 
                  linetype = median.line.type, 
-                 size = 1.5) +
+                 size = median.line.size) +
       geom_vline(xintercept = x_mode, 
                  colour = mode.line.color, 
-                 linetype = mode.line.type)
+                 linetype = mode.line.type,
+                 size = mode.line.size)
     print(ppp)
   }
-  return(summary(x))
+  # build the second plot
+  if ("v" %in% tolower(whatplots)) {
+    pppp <- ggplot() +
+      aes(x) +
+      labs(
+        title = my_title,
+        subtitle = my_subtitle,
+        y = xlab,
+        caption = (bquote(bar(X) ~ " displayed as a red dot, Median as a black diamond, and outlier(s) as small dark red dots"))
+      ) +
+      geom_violin(aes(x = "", 
+                       y = x), 
+                   fill = data.fill.color) +
+      geom_jitter(aes(x = "", 
+                      y = x),
+                  width = 0.05, 
+                  height = 0,
+                  alpha = .5) +
+      coord_flip() +
+      geom_point(aes(x = "", 
+                     y = x_mean), 
+                 shape = mean.point.shape, 
+                 size = mean.point.size, 
+                 color = "white", 
+                 fill = mean.line.color) +
+      geom_point(aes(x = "", 
+                     y = x_median), 
+                 shape = median.point.shape, 
+                 size = median.point.size, 
+                 color = "white", 
+                 fill = median.line.color) +
+      theme(
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.line.y = element_blank(),
+        panel.grid.major.y = element_blank()
+      )
+    print(pppp)
+  }
+  text.output <- DescTools::Desc(x, plotit = FALSE, main = xlab, digits = k)
+  return(text.output)
 } # end function
